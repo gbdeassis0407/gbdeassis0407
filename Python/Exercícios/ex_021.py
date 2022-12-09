0a9ff13bc9e78c605399f3bfbd6356f21a5b2774
Guilherme Borges de Assis <gbdeassis@hotmail.com.br>
2022-12-09 11:36:14 -0300

    Subindo exercícios 021 e 022
    Subindo exercícios 021 e 022 junto com audio para exercícios 021 pygame

---
+++ Python/Exercícios/ex_021.py
@@ -0,0 +1,6 @@
+# Por favor utilizar versão 3.9.10 python, para suporte a biblioteca python
+import pygame
+pygame.init()
+pygame.mixer.music.load("ex_021.mp3")
+pygame.mixer.music.play()
+pygame.event.wait()
